6e051ef2540ef23b908a29708ae653111f980146
Gouldian0120 <86667277+Gouldian0120@users.noreply.github.com>
2021-04-04 19:38:53 -0300

    - Contract storage optimizations

--- contracts/finance/RedeemableToken.sol
+++ contracts/finance/RedeemableToken.sol
@@ -13,14 +13,14 @@ abstract contract RedeemableToken is ERC20 {
 
     function redeemAllowed() virtual public returns(bool);
 
-    function redeem(address owner) external returns (uint value) {
+    function redeem(address owner) external returns (uint) {
 
         address[] memory owners = new address[](1);
         owners[0] = owner;
-        value = redeem(owners);
+        redeem(owners);
     }
 
-    function redeem(address[] memory owners) public returns (uint value) {
+    function redeem(address[] memory owners) public returns (uint) {
 
         require(redeemAllowed(), "redeemd not allowed");
 
@@ -32,7 +32,6 @@ abstract contract RedeemableToken is ERC20 {
         for (uint i = 0; i < owners.length; i++) {
             if (owners[i] != address(0)) {
                 (uint bal, uint val) = redeem(valTotal, supplyTotal, owners[i]);
-                value = value.add(val);
                 valRemaining = valRemaining.sub(val);
                 supplyRemaining = supplyRemaining.sub(bal);
             }
